b5062acbfd4a395b40511da85b796b0ad0524cda
annemeinertz <32507638+annemeinertz@users.noreply.github.com>
2019-05-24 03:11:00 +0200

    Delete burger-menu-script.js

--- burger-menu-script.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.toggle-nav').click(function(e) {
-        jQuery('.menusvommehal ul').slideToggle(500);
- 
-        e.preventDefault();
-    });
-    
-});
-
